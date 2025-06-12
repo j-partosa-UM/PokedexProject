ac802044a8cef0a5839d1667ffdbd5b5686cf434
J <j.partosaii.523674@umindanao.edu.ph>
2025-06-12 11:06:49 +0800

    Initial commit

---
+++ routes/pokemon.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import axios from 'axios';
+import env from 'dotenv';
+
+env.config();
+
+const router = express.Router();
+const API_URL = process.env.API_URL;
+
+router.get('/', async (req, res) => {
+    try {
+        const response = await axios.get(API_URL);
+        res.render('pages/home', { pokemonList: response.data.results });
+    } catch (error) {
+        res.status(500).send('Error fetching Pokémon data');
+    }
+})
+
+router.get('/pokemon/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
+        res.render('pages/details', { pokemon: response.data });
+    } catch (error) {
+        res.status(500).send('Error fetching Pokémon data');
+    }
+})
+
+export default router;
